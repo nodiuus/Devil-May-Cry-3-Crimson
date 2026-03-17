098d923fc7b8906f5088678771a0e4a2d2881ec4
Berth <miaberthrage@gmail.com>
2026-03-17 19:49:57 -0300

    Fix: Breakpoints Crashing no more

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -307,5 +307,5 @@ void UpdateFrameRate() {
     *frequencyAddr = (defaultFrequency * static_cast<double>(g_frameRateMultiplier));
 
     // This affects the missionData.frameCount value
-    *frameRateAddr = static_cast<int32>(60.0 * multiplier);
+    *frameRateAddr = static_cast<int32>(activeConfig.frameRate);
 }
