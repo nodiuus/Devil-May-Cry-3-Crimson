dcadde871ae21d291318bb2b5d75164f722250d4
Berth <miaberthrage@gmail.com>
2026-04-28 17:45:15 -0300

    Feat: FixWarpEffect HighFPS Speed

--- src/masm/FixBlendingEffects.asm
+++ src/masm/FixBlendingEffects.asm
@@ -1,54 +1,111 @@
 INCLUDE CommonMacros.inc
 .DATA
+EXTERN g_FrameRateTimeMultiplier:DWORD
+
 EXTERN g_FixBlendingEffects_Mist_ReturnAddr1:QWORD
 EXTERN g_FixBlendingEffects_Mist_ReturnAddr2:QWORD
 EXTERN g_FixBlendingEffects_Mist_ReturnAddr3:QWORD
+EXTERN g_MistBlendingEffect_SpdAddr1:QWORD
+EXTERN g_MistBlendingEffect_SpdAddr2:QWORD
+
 EXTERN g_FixBlendingEffects_Warp_ReturnAddr1:QWORD
 EXTERN g_FixBlendingEffects_Warp_ReturnAddr2:QWORD  
 
 .CODE
 FixBlendingEffect_Mist_Detour1 PROC
     push rax
-    sub rsp, 20h ; Reserving shadow-space for xmm
-    movdqu [rsp], xmm0 ; Save xmm0
-    movdqu [rsp + 10h], xmm1 ; Save xmm1
+    mov rax, g_MistBlendingEffect_SpdAddr1
 
-    ;mov rax, g_FrameRateTimeMultiplier
-   ; movss xmm0, rax
+    jmp ApplyNewMistSpeed
 
-    movsx eax, r9w ; r9w contains the original turn rate value (16-bit), move it to eax with sign extension
-    cvtsi2ss xmm1, eax ; Convert the turn rate value to float in xmm1
+ApplyNewMistSpeed:
+    movss xmm1, dword ptr [rax]
+    mulss xmm1, dword ptr [g_FrameRateTimeMultiplier] 
 
-   ; mulss xmm1, dword ptr [g_FrameRateTimeMultiplier] 
-
-;    mov rax, [g_FasterTurnRateSettingAddr] ; Get pointer to fasterTurnRate
-    cmp byte ptr [rax], 1
-    je TurnRateFasterFix
-    jmp TurnRateFix
-
-
-TurnRateFasterFix:
-    ;mulss xmm1, [fasterTurnRateMultiplier]
-    cvtss2si eax, xmm1 ; Convert the modified turn rate back to integer in eax
-    mov r9w, ax
-    jmp OriginalCode
-
-TurnRateFix:
-    cvtss2si eax, xmm1 
-    mov r9w, ax
-    jmp OriginalCode
-
-
-OriginalCode:
-    movsx ebx, r9w    ; Apply Turn Rate
-    movdqu xmm0, [rsp]
-    movdqu xmm1, [rsp + 10h]
-
-    add rsp, 20h 
     pop rax
-    movsx ecx, word ptr [r8]
-    ;jmp qword ptr [g_TurnRateFix_ReturnAddr]
 
+    jmp qword ptr [g_FixBlendingEffects_Mist_ReturnAddr1]
 
 FixBlendingEffect_Mist_Detour1 ENDP
+
+
+.CODE
+FixBlendingEffect_Mist_Detour2 PROC
+    push rax
+    mov rax, g_MistBlendingEffect_SpdAddr2
+
+    jmp ApplyNewMistSpeed
+
+ApplyNewMistSpeed:
+    mulss xmm0, dword ptr [rax]
+    mulss xmm0, dword ptr [g_FrameRateTimeMultiplier] 
+
+    pop rax
+
+    jmp qword ptr [g_FixBlendingEffects_Mist_ReturnAddr2]
+
+FixBlendingEffect_Mist_Detour2 ENDP
+
+
+.CODE
+FixBlendingEffect_Mist_Detour3 PROC
+    push rax
+    mov rax, g_MistBlendingEffect_SpdAddr2
+
+    jmp ApplyNewMistSpeed
+
+ApplyNewMistSpeed:
+    mulss xmm6, dword ptr [rax]
+    mulss xmm6, dword ptr [g_FrameRateTimeMultiplier] 
+
+    pop rax
+
+    jmp qword ptr [g_FixBlendingEffects_Mist_ReturnAddr3]
+
+FixBlendingEffect_Mist_Detour3 ENDP
+
+
+.CODE
+FixBlendingEffect_Warp_Detour1 PROC
+    sub rsp, 20h ; Reserving shadow-space for xmm
+    movdqu [rsp], xmm1 ; Save xmm1
+
+    mov eax, [rsi+20h]
+    cvtsi2ss  xmm1, eax ; Convert the turn rate value to float in xmm1
+    mulss xmm1, dword ptr [g_FrameRateTimeMultiplier]
+
+    jmp ApplyNewWarpSpeed
+
+ApplyNewWarpSpeed:
+    cvttss2si eax, xmm1
+    mov edx,ebx
+
+    movdqu xmm1, [rsp]
+    add rsp, 20h
+    jmp qword ptr [g_FixBlendingEffects_Warp_ReturnAddr1]
+
+FixBlendingEffect_Warp_Detour1 ENDP
+
+
+.CODE
+FixBlendingEffect_Warp_Detour2 PROC
+    sub rsp, 20h ; Reserving shadow-space for xmm
+    movdqu [rsp], xmm1 ; Save xmm1
+
+    mov eax, [rsi+24h]
+    cvtsi2ss  xmm1, eax ; Convert the turn rate value to float in xmm1
+    mulss xmm1, dword ptr [g_FrameRateTimeMultiplier]
+
+    jmp ApplyNewWarpSpeed
+
+ApplyNewWarpSpeed:
+    cvttss2si eax, xmm1
+    add [rsi+74h],eax
+
+    movdqu xmm1, [rsp]
+    add rsp, 20h
+    jmp qword ptr [g_FixBlendingEffects_Warp_ReturnAddr2]
+
+FixBlendingEffect_Warp_Detour2 ENDP
+
 END
